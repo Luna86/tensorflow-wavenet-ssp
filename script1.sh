03e601de2be7047085524ad84268866a3a744c1e
Luna <yangluonaluna@gmail.com>
2017-01-21 12:22:19 -0500

    rm script

--- script1.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train_skeleton.py --batch_size=50 --data_dir=/home/lunay/data/pose/training \
---checkpoint_every=1000 --num_steps=65000 --sample_size=100 --learning_rate=1e-3 --optimizer=adam \
---histograms=False --logdir_root=/media/posefs1b/Users/luna/wavenet --wavenet_params=./wavenet_params1.json --epsilon=1e-8
--- script4.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=3 python train_skeleton.py --batch_size=50 --data_dir=/home/lunay/data/pose/training \
---checkpoint_every=1000 --num_steps=65000 --sample_size=500 --learning_rate=1e-4 --optimizer=adam \
---histograms=False --logdir_root=/media/posefs1b/Users/luna/wavenet --wavenet_params=./wavenet_params4.json
